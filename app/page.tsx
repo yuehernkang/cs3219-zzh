1ada3d2420a1f07e683f47a84d2851f7cf58a59f
Yue Hern <yuehernkang@hotmail.com>
2024-11-08 06:23:20 +0800

    Added code editor

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,64 @@
+"use client"
+import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
+import { useState } from 'react';
 
 export default function Home() {
+  const [code, setCode] = useState<string>();
+  const handleSubmit = async (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://34.87.60.94:2358/submissions/?base64_encoded=false&wait=true', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            source_code: 'print("Hello, World!")', // The code to execute
+            language_id: 71, // Language ID for Python 3; change as needed
+            stdin: '', // Input to be given to the code
+          }
+        ),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json();
+      console.log('Success:', data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-   <div>hello</div>
+    <div className="flex justify-center items-start pt-10 h-screen">
+      <div className="w-full max-w-4xl p-4 border">
+        <form action="#" onSubmit={handleSubmit}>
+          <div className="">
+            <label htmlFor="comment" className="sr-only">
+              Add your code
+            </label>
+            <Editor
+            value={code}
+              height="50vh"
+              defaultLanguage="javascript"
+              defaultValue='Deno.serve(req => new Response("Hello!"));'
+            />
+          </div>
+          <div className="flex justify-between pt-2">
+            <div className="flex items-center space-x-5"></div>
+            <div className="flex-shrink-0">
+              <button
+                type="submit"
+                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500"
+              >
+                Run
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    return config
+  }
 };
 
 export default nextConfig;
